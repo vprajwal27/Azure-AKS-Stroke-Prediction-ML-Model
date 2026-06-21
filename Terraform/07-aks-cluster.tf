7dcb3af9b4d13d81708a126b26721941bc83fdff
Prajwal <prith.prajju@gmail.com>
2026-06-21 16:20:13 -0300

    v3

--- Terraform/07-aks-cluster.tf
+++ Terraform/07-aks-cluster.tf
@@ -9,7 +9,7 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   default_node_pool {
     name                 = "systempool"
     node_count           = var.environment == "prod" ? 2 : 1 # prod gets 2 nodes, staging 1
-    vm_size              = "Standard_B2s"                    
+    vm_size              = "Standard_D2s_v3"                   
     vnet_subnet_id       = azurerm_subnet.aks_subnet.id
     orchestrator_version = data.azurerm_kubernetes_service_versions.current.latest_version
     os_disk_size_gb      = 30
@@ -22,6 +22,8 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   network_profile {
     network_plugin    = "azure" # Azure CNI
     load_balancer_sku = "standard"
+    service_cidr      = "10.1.0.0/16"
+    dns_service_ip    = "10.1.0.10"
   }
 
   tags = {
